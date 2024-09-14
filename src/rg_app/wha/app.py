058f9411acc5eb30b9773b8aaab4ac8c3d11c05c
Mikołaj Nowak <12396461+m3nowak@users.noreply.github.com>
2024-09-14 16:18:14 +0200

    Change index

--- src/rg_app/wha/app.py
+++ src/rg_app/wha/app.py
@@ -1,6 +1,6 @@
 import msgspec
 from litestar import Litestar, get, post
-from litestar.exceptions import PermissionDeniedException
+from litestar.exceptions import PermissionDeniedException, ServiceUnavailableException
 from litestar.params import Parameter
 from nats.aio.client import Client as NatsClient
 from nats.js import JetStreamContext
@@ -41,16 +41,10 @@ async def webhook_handler(
 
 @get("/")
 async def index(nats: NatsClient, js: JetStreamContext) -> str:
-    hello = "Hello, world!"
-    await nats.publish("hello", hello.encode())
-    kv = await js.key_value("hello")
-    try:
-        entry = await kv.get("hello")
-        current_val = int(entry.value.decode() if entry.value else 0)
-    except KeyNotFoundError:
-        current_val = 0
-    await kv.put("hello", str(current_val + 1).encode())
-    return f"Hello, world!, {current_val}"
+    if nats.is_connected:
+        return "Server ready"
+    else:
+        raise ServiceUnavailableException("NATS connection is not ready")
 
 
 def app_factory(config: Config, debug_mode: bool = False, no_register: bool = False) -> Litestar:
