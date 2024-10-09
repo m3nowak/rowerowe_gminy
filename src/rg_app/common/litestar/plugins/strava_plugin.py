1022656fa82205cbca34d10483a3396061fabf17
Mikołaj Nowak <12396461+m3nowak@users.noreply.github.com>
2024-10-09 22:06:53 +0200

    remove unused strava api autogen reference

--- src/rg_app/common/litestar/plugins/strava_plugin.py
+++ src/rg_app/common/litestar/plugins/strava_plugin.py
@@ -1,12 +1,9 @@
-from contextlib import asynccontextmanager
 from dataclasses import dataclass
-from typing import AsyncGenerator
 
 from httpx import AsyncClient
 from litestar.config.app import AppConfig
 from litestar.di import Provide
 from litestar.plugins import InitPluginProtocol
-from strava_api import Client as StravaClient
 
 from rg_app.common.strava import StravaTokenManager
 
@@ -31,27 +28,11 @@ class StravaPlugin(InitPluginProtocol):
         self._token_mgr = StravaTokenManager(sp_cfg.client_id, sp_cfg.client_secret)
         self._client: AsyncClient | None = None
 
-    @asynccontextmanager
-    async def client_ctx(self) -> AsyncGenerator[AsyncClient, None]:
-        strava_client = StravaClient(self.cfg.url)
-        async with strava_client.get_async_httpx_client() as client:
-            try:
-                self._client = client
-                yield client
-            finally:
-                pass
-
     def on_app_init(self, app_config: AppConfig) -> AppConfig:
         app_config.lifespan.append(self._token_mgr.begin())
-        app_config.lifespan.append(self.client_ctx())
 
         def provide_token_mgr() -> StravaTokenManager:
             return self._token_mgr
 
-        def provide_client() -> AsyncClient:
-            assert self._client is not None
-            return self._client
-
         app_config.dependencies[self.cfg.token_mgr_kwarg] = Provide(provide_token_mgr, sync_to_thread=False)
-        app_config.dependencies[self.cfg.api_client_kwarg] = Provide(provide_client, sync_to_thread=False)
         return app_config
