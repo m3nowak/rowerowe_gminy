be03488e1b49eb22802df10549bd3fc258d783d9
Mikołaj Nowak <12396461+m3nowak@users.noreply.github.com>
2024-10-29 21:43:52 +0100

    Disable msg deduplication in incoming-wha

--- src/rg_app/nats_defs/cloud.py
+++ src/rg_app/nats_defs/cloud.py
@@ -7,7 +7,7 @@ STREAM_INCOMING_WHA = StreamConfig(
     name="incoming-wha",
     description="Strava incoming webhooks",
     subjects=["rg.incoming.wha.*.*"],
-    max_msgs_per_subject=1,
+    # max_msgs_per_subject=1,
     max_bytes=1 * (1024**3),  # 1GB
 )
 
