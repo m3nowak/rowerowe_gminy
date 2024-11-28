cccfaaa14cccfa9233a1d7c13730111716d92656
Mikołaj Nowak <12396461+m3nowak@users.noreply.github.com>
2024-11-28 21:59:02 +0100

    Polish up simplification code

--- src/rg_app/geo/__init__.py
+++ src/rg_app/geo/__init__.py
@@ -1,12 +1,13 @@
+import gzip
+
 from rg_app.geo.simplification import toposimplify_duckdb
 
 # from rg_app.geo.src_load import load_gml_files
 
 if __name__ == "__main__":
     duck_path = "data/geo.db"
-    sb500 = toposimplify_duckdb(duck_path, 0.002)
     sb200 = toposimplify_duckdb(duck_path, 0.005)
-    # sb500.to_file("data/dgs2e_3.json", driver="GeoJSON")
-    # sb100.to_file("data/dgs1e_2.json", driver="GeoJSON")
-    # sb500.to_file("data/dgs2e_3.topojson", driver="GeoJSON")
-    # sb100.to_file("data/dgs1e_2.topojson", driver="GeoJSON")
+    with open("data/tpy5e_3.json", "w") as f:
+        f.write(sb200)
+    with gzip.open("data/tpy5e_3.json.gz", "wt") as f:
+        f.write(sb200)
--- src/rg_app/geo/simplification.py
+++ src/rg_app/geo/simplification.py
@@ -5,30 +5,26 @@ import geopandas as gpd
 import topojson as tp
 
 
-def toposimplify_duckdb(path: str, toposimplify: float) -> gpd.GeoDataFrame:
+def toposimplify_duckdb(path: str, toposimplify: float) -> str:
     """Create a topology to ensure proper boundaries between regions and simplify it."""
     # shamelessly stolen from https://github.com/kraina-ai/srai/blob/main/srai%2Fregionalizers%2Fadministrative_boundary_regionalizer.py#L313-L332
-    conn = duckdb.connect(path)
-    conn.load_extension("spatial")
+    with duckdb.connect(path) as conn:
+        conn.load_extension("spatial")
 
-    df = conn.execute(
-        "SELECT borders.ID , ST_AsText(borders.shape) as shape, type, parent_id FROM borders WHERE type='GMI'"  # WHERE type='WOJ' OR type='PAN'
-    ).df()
+        df = conn.execute(
+            "SELECT borders.ID , ST_AsText(borders.shape) as shape, type, parent_id FROM borders WHERE type='GMI'"  # WHERE type='WOJ' OR type='PAN'
+        ).df()
 
-    gdf = gpd.GeoDataFrame(data=df, geometry=gpd.GeoSeries.from_wkt(df["shape"]), crs="EPSG:4326")  # type: ignore
+        gdf = gpd.GeoDataFrame(data=df, geometry=gpd.GeoSeries.from_wkt(df["shape"]), crs="EPSG:4326")  # type: ignore
 
-    gdf.drop(columns=["shape"], inplace=True)
+        gdf.drop(columns=["shape"], inplace=True)
 
-    topo = tp.Topology(
-        gdf,
-        prequantize=1e6,  # type: ignore
-        presimplify=False,
-        toposimplify=toposimplify,  # type: ignore
-        simplify_algorithm="dp",
-        prevent_oversimplify=True,
-    )
-    topo.to_json(f"data/topo{str(toposimplify).replace('.','_')}.json")
-    gdf = ty.cast(gpd.GeoDataFrame, topo.to_gdf(winding_order="CW_CCW", validate=True))
-    conn.close()
-
-    return gdf
+        topo = tp.Topology(
+            gdf,
+            prequantize=1e6,  # type: ignore
+            presimplify=False,
+            toposimplify=toposimplify,  # type: ignore
+            simplify_algorithm="dp",
+            prevent_oversimplify=True,
+        )
+        return ty.cast(str, topo.to_json())
