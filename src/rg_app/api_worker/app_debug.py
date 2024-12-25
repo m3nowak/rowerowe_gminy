1f31d7a8a86d3c892f731fc06b9a1ed43f83ce3f
Mikołaj Nowak <min@dsr.com.pl>
2024-12-25 19:36:45 +0100

    Add debug_app_factory function to utilize app_factory with debug configuration

--- src/rg_app/api_worker/app_debug.py
+++ src/rg_app/api_worker/app_debug.py
@@ -1,5 +1,6 @@
 import os
 
+from .app import app_factory
 from .config import Config
 
 ENV_KEY = "API_WORKER_CFG"
@@ -10,3 +11,8 @@ def get_debug_config():
     print(f"Debug mode, using config ftom env ({ENV_KEY}) or default ({DEFAULT_CFG})")
     cfg_path = os.environ.get(ENV_KEY, DEFAULT_CFG)
     return Config.from_file(cfg_path)
+
+
+def debug_app_factory():
+    config = get_debug_config()
+    return app_factory(config)
