880cb03722302ccf70dc4d6dee412f7a40211000
Mikołaj Nowak <12396461+m3nowak@users.noreply.github.com>
2025-03-27 22:51:45 +0100

    Dead code removal

--- src/rg_app/worker/routers/webhook_activities.py
+++ src/rg_app/worker/routers/webhook_activities.py
@@ -42,36 +42,3 @@ async def activities_handle(
     subject = internal_cmd_activity_subject(msg.type, msg.owner_id, msg.activity_id)
     await publisher.publish(msg, subject)
     await nats_msg.ack()
-
-
-# @router.subscriber(
-#     config=CONSUMER_REVOCATIONS,
-#     stream=wha_stream,
-#     durable=CONSUMER_REVOCATIONS.durable_name,
-#     pull_sub=PullSub(batch_size=1),
-#     no_ack=True,
-#     # title=f"{wha_stream.name}/{CONSUMER_REVOCATIONS.durable_name}",
-# )
-# async def revocations_handle(
-#     body: WebhookAthlete,
-#     # broker: NatsBroker,
-#     # nats_msg: NatsMessage,
-#     # session: AsyncSession = Depends(db_session),
-# ):
-#     print("Got revocation for: ", body.object_id)
-#     if body.aspect_type != "delete":
-#         print("Unknown aspect type for revocation")
-#         # await nats_msg.ack()
-#         return
-#     # Will delete user
-#     # nats_conn = broker._connection
-#     # assert nats_conn is not None
-#     # js = nats_conn.jetstream()
-#     # activity_stream_name = ty.cast(str, STREAM_ACTIVITY_CMD.name)
-#     # await js.purge_stream(activity_stream_name, subject=f"rg.internal.cmd.activity.*.{body.object_id}.>")
-#     # user = await session.get(User, body.object_id)
-#     # if user:
-#     #     await session.delete(user)
-#     # await session.commit()
-#     # await nats_msg.ack()
-#     print(f"User {body.object_id} deleted")
